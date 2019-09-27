2e11b8dc560aa79193cf3cbf281514f3b0b60bba
Martin Ford <ford.j.martin@gmail.com>
2019-09-27 09:51:56 +0100

    Add Assumptions

--- lib/tab_view/profile_creation/mqq_assumptions.dart
+++ lib/tab_view/profile_creation/mqq_assumptions.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:motor_quick_quote/common/branded_app_bar.dart';
+import 'package:motor_quick_quote/common/styling.dart';
 
 import '../mqq_main_tab_view.dart';
 
@@ -8,16 +10,93 @@ class MQQAssumptionsPage extends StatefulWidget {
 }
 
 class _MQQAssumptionsPageState extends State<MQQAssumptionsPage> {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold( body: Container(child: Center(child: MaterialButton(child: Text("Accept Assumptions"), onPressed: () { _acceptAssumptions(); },) )));
-    
-      }
-    
-      void _acceptAssumptions() {
-        print("assumptions accepted");
-            Navigator.push(context, new MaterialPageRoute(
-            builder: (context) => MQQMainTabViewPage()
-          ));
-      }
+    return Scaffold(
+      appBar: new BrandedAppBar(),
+      body: Container(
+        color: Styling.secondary100,
+        child: ListView(
+            children: <Widget>[
+              Container(
+                  margin: EdgeInsets.all(20),
+                  child: SizedBox(
+                      width: MediaQuery.of(context).size.width * .8,
+                      child: Text(
+                        "Quote Assumptions",
+                        textAlign: TextAlign.center,
+                        style: Styling.getTestDrivePageHeaderTextStyle(context),
+                      ))),
+              ListTile(
+                title: Text('Cover starts', style: titleStyle(),),
+                subtitle: Text('Firday 27 September 2019', style: subtitleStyle(),),
+              ),
+              ListTile(
+                title: Text('Level of cover', style: titleStyle(),),
+                subtitle: Text('COMPREHENSIVE', style: subtitleStyle(),),
+              ),
+              ListTile(
+                title: Text('Class of use', style: titleStyle(),),
+                subtitle: Text('Domestic & commuting', style: subtitleStyle(),),
+              ),
+              ListTile(
+                title: Text('Voluntary excess', style: titleStyle(),),
+                subtitle: Text('£150', style: subtitleStyle(),),
+              ),
+              ListTile(
+                title: Text('Annual milage', style: titleStyle(),),
+                subtitle: Text('upto 10,000 miles', style: subtitleStyle(),),
+              ),
+              ListTile(
+                title: Text('Overnight location', style: titleStyle(),),
+                subtitle: Text('Drivers postcode', style: subtitleStyle(),),
+              ),
+              Container(
+                child: new Padding(
+                    padding: EdgeInsets.fromLTRB(10.0, 45.0, 10.0, 0.0),
+                    child: SizedBox(
+                      height: 40.0,
+                      child: new RaisedButton(
+                        elevation: 5.0,
+                        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                        color: Colors.blue,
+                        child: new Text('Accept', style: new TextStyle(fontSize: 20.0, color: Colors.white)),
+                        onPressed: _acceptAssumptions,
+                      ),
+                    )),
+              ),
+              Container(
+                child: new Padding(
+                    padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 0.0),
+                    child: SizedBox(
+                      height: 40.0,
+                      child: new RaisedButton(
+                        elevation: 5.0,
+                        shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                        color: Colors.blue,
+                        child: new Text('Edit', style: new TextStyle(fontSize: 20.0, color: Colors.white)),
+                      ),
+                    )),
+              ),
+            ],
+          ),
+      )
+    );
+  }
+
+  void _acceptAssumptions() {
+    print("assumptions accepted");
+    Navigator.push(context, new MaterialPageRoute(
+        builder: (context) => MQQMainTabViewPage()
+    ));
+  }
+
+  TextStyle titleStyle() {
+    return TextStyle(color: Colors.black.withOpacity(0.5), fontWeight: FontWeight.normal,);
+  }
+
+  TextStyle subtitleStyle() {
+    return TextStyle(color: Colors.black.withOpacity(1.0), fontWeight: FontWeight.normal, fontSize: 18.0,);
+  }
 }
--- lib/tab_view/profile_creation/mqq_dln_results.dart
+++ lib/tab_view/profile_creation/mqq_dln_results.dart
@@ -11,6 +11,88 @@ class MQQDriverLicenceResultsPage extends StatefulWidget {
 
 class _MQQDriverLicenceResultsPageState extends State<MQQDriverLicenceResultsPage> {
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new BrandedAppBar(),
+      body: Container(
+        color: Styling.secondary100,
+        child: ListView(
+          children: <Widget>[
+            Container(
+                margin: EdgeInsets.all(20),
+                child: SizedBox(
+                    width: MediaQuery.of(context).size.width * .8,
+                    child: Text(
+                      "Driving License Details",
+                      textAlign: TextAlign.center,
+                      style: Styling.getTestDrivePageHeaderTextStyle(context),
+                    ))),
+            ListTile(
+              title: Text('Name', style: titleStyle(),),
+              subtitle: Text('MORGAN', style: subtitleStyle(),),
+            ),
+            ListTile(
+              title: Text('First Name', style: titleStyle(),),
+              subtitle: Text('SARAH MEREDYTH', style: subtitleStyle(),),
+            ),
+            ListTile(
+              title: Text('Date and place of birth', style: titleStyle(),),
+              subtitle: Text('11.03.1976 UNITED KINGDOM', style: subtitleStyle(),),
+            ),
+            ListTile(
+              title: Text('Date of issue', style: titleStyle(),),
+              subtitle: Text('19.01.2013', style: subtitleStyle(),),
+            ),
+            ListTile(
+              title: Text('Date of expiry', style: titleStyle(),),
+              subtitle: Text('18.01.2023', style: subtitleStyle(),),
+            ),
+            ListTile(
+              title: Text('Issued by', style: titleStyle(),),
+              subtitle: Text('DVLA', style: subtitleStyle(),),
+            ),
+            ListTile(
+              title: Text('Licence number', style: titleStyle(),),
+              subtitle: Text('MORGA753116SM9IL 35', style: subtitleStyle(),),
+            ),
+            ListTile(
+              title: Text('Address', style: titleStyle(),),
+              subtitle: Text('122 BURNS CRESCENT EDINGURGH EH1 9GP', style: subtitleStyle(),),
+            ),
+            Container(
+              child: new Padding(
+                  padding: EdgeInsets.fromLTRB(10.0, 45.0, 10.0, 0.0),
+                  child: SizedBox(
+                    height: 40.0,
+                    child: new RaisedButton(
+                      elevation: 5.0,
+                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                      color: Colors.blue,
+                      child: new Text('Confirm', style: new TextStyle(fontSize: 20.0, color: Colors.white)),
+                      onPressed: _confirm,
+                    ),
+                  )),
+            ),
+            Container(
+              child: new Padding(
+                  padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 0.0),
+                  child: SizedBox(
+                    height: 40.0,
+                    child: new RaisedButton(
+                      elevation: 5.0,
+                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                      color: Colors.blue,
+                      child: new Text('Edit', style: new TextStyle(fontSize: 20.0, color: Colors.white)),
+                    ),
+                  )),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+
   // Confirm
   void _confirm() async {
     try {
@@ -22,72 +104,11 @@ class _MQQDriverLicenceResultsPageState extends State<MQQDriverLicenceResultsPag
     }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: new BrandedAppBar(),
-      body: ListView(
-      children: <Widget>[
-        ListTile(
-          title: Text('Name', style: TextStyle(color: Colors.black.withOpacity(0.5), fontWeight: FontWeight.normal,),),
-          subtitle: Text('SMITH', style: TextStyle(color: Colors.black.withOpacity(1.0), fontWeight: FontWeight.normal, fontSize: 24.0,),),
-        ),
-        ListTile(
-          title: Text('First Name', style: TextStyle(color: Colors.black.withOpacity(0.5), fontWeight: FontWeight.normal,),),
-          subtitle: Text('ALICE BARBARA', style: TextStyle(color: Colors.black.withOpacity(1.0), fontWeight: FontWeight.normal, fontSize: 24.0,),),
-        ),
-        ListTile(
-          title: Text('Date and place of birth', style: TextStyle(color: Colors.black.withOpacity(0.5), fontWeight: FontWeight.normal,),),
-          subtitle: Text('01.04.1984 WALES', style: TextStyle(color: Colors.black.withOpacity(1.0), fontWeight: FontWeight.normal, fontSize: 24.0,),),
-        ),
-        ListTile(
-          title: Text('Date of issue', style: TextStyle(color: Colors.black.withOpacity(0.5), fontWeight: FontWeight.normal,),),
-          subtitle: Text('04.05.2012', style: TextStyle(color: Colors.black.withOpacity(1.0), fontWeight: FontWeight.normal, fontSize: 24.0,),),
-        ),
-        ListTile(
-          title: Text('Date of expiry', style: TextStyle(color: Colors.black.withOpacity(0.5), fontWeight: FontWeight.normal,),),
-          subtitle: Text('03.05.2022', style: TextStyle(color: Colors.black.withOpacity(1.0), fontWeight: FontWeight.normal, fontSize: 24.0,),),
-        ),
-        ListTile(
-          title: Text('Issued by', style: TextStyle(color: Colors.black.withOpacity(0.5), fontWeight: FontWeight.normal,),),
-          subtitle: Text('DVLA', style: TextStyle(color: Colors.black.withOpacity(1.0), fontWeight: FontWeight.normal, fontSize: 24.0,),),
-        ),
-        ListTile(
-          title: Text('Licence number', style: TextStyle(color: Colors.black.withOpacity(0.5), fontWeight: FontWeight.normal,),),
-          subtitle: Text('SMIT9804014AB5TV 03', style: TextStyle(color: Colors.black.withOpacity(1.0), fontWeight: FontWeight.normal, fontSize: 24.0,),),
-        ),
-        ListTile(
-          title: Text('Address', style: TextStyle(color: Colors.black.withOpacity(0.5), fontWeight: FontWeight.normal,),),
-          subtitle: Text('TY GWYN, 32 FAIRWATER ROAD, CARDIFF, CF5 2LE', style: TextStyle(color: Colors.black.withOpacity(1.0), fontWeight: FontWeight.normal, fontSize: 24.0,),),
-        ),
-        Container(
-          child: new Padding(
-              padding: EdgeInsets.fromLTRB(10.0, 45.0, 10.0, 0.0),
-              child: SizedBox(
-                height: 40.0,
-                child: new RaisedButton(
-                  elevation: 5.0,
-                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-                  color: Colors.blue,
-                  child: new Text('Confirm', style: new TextStyle(fontSize: 20.0, color: Colors.white)),
-                  onPressed: _confirm,
-                ),
-              )),
-        ),
-        Container(
-          child: new Padding(
-              padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 0.0),
-              child: SizedBox(
-                height: 40.0,
-                child: new RaisedButton(
-                  elevation: 5.0,
-                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-                  color: Colors.blue,
-                  child: new Text('Edit', style: new TextStyle(fontSize: 20.0, color: Colors.white)),
-                ),
-              )),
-        ),
-      ],
-    ),);
+  TextStyle titleStyle() {
+    return TextStyle(color: Colors.black.withOpacity(0.5), fontWeight: FontWeight.normal,);
+  }
+
+  TextStyle subtitleStyle() {
+    return TextStyle(color: Colors.black.withOpacity(1.0), fontWeight: FontWeight.normal, fontSize: 18.0,);
   }
 }
